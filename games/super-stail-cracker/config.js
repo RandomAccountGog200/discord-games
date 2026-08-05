3931cd95cfdcc8e04a361ce7b3d9699059552e61
RandomAccountGog200 <gognomthecheese@yahoo.com>
2026-08-05 14:02:47 -0700

    Add game: Super Stail Cracker (config.js)

---
+++ games/super-stail-cracker/config.js
@@ -0,0 +1,13 @@
+export const COLORS = {
+  bg: '#17142d',
+  grid: 'rgba(255, 244, 210, .055)',
+  cream: '#fff4d2',
+  gold: '#ffc857',
+  orange: '#ef8354',
+  pink: '#ef476f',
+  blue: '#4cc9f0',
+  purple: '#9b5de5',
+  green: '#83d483'
+};
+export const FIXED_STEP = 1 / 60;
+export const MAX_WAVE = 8;
