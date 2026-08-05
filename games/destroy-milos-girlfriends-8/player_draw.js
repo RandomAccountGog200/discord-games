7afa0c2c058e173dfb8ee8066a0e28add9c261b1
RandomAccountGog200 <gognomthecheese@yahoo.com>
2026-08-04 21:06:11 -0700

    Add game: Destroy Milos Girlfriends 8 (player_draw.js)

---
+++ games/destroy-milos-girlfriends-8/player_draw.js
@@ -0,0 +1,5 @@
+// Extend Player with draw method (we'll add to player.js to keep together)
+// Instead, we'll add draw method directly in player.js
+// This file is placeholder for modularity, but we'll just include in player.js
+// Let's modify player.js to include draw.
+// We'll update player.js to add draw function.
